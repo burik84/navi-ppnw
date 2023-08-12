292aefcf951c1a06fe0124c31cc58fe21dd30bda
burik84 <avburik84@gmail.com>
2023-08-12 22:56:57 +0300

    fix: fixed button up

--- src/script/components/ButtonUp.tsx
+++ src/script/components/ButtonUp.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
+import {useState} from 'react';
 
 import { IconButton } from '@mui/material';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
-import { useWindowDimensions, useHandleScroll } from '../services/WindowDimensions';
+// import { useWindowDimensions, useHandleScroll } from '../services/WindowDimensions';
 
 export const ButtonUp: React.FC = () => {
-  const { height } = useWindowDimensions();
-  const scrollY = useHandleScroll();
+  // const { height } = useWindowDimensions();
+  // const scrollY = useHandleScroll();
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300){
+      setVisible(true)
+    }
+    else if (scrolled <= 300){
+      setVisible(false)
+    }
+  };
 
   const handleUp = () => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   };
+
+  window.addEventListener('scroll', toggleVisible);
 
   return (
     <div className="absolute">
       <IconButton
-        className={scrollY >= 10 ? 'button button__up active' : 'button button__up'}
+        className={visible ? 'button button__up active' : 'button button__up'}
         aria-label="наверх"
         onClick={handleUp}
       >
